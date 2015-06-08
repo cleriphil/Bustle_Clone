746aec7a45d1638a3cb53d5fec9c77b98eb79d9e
Peter & cleri philiastides <student@epicodus.com>
2015-06-08 16:19:09 -0700

    bootstrap carousel and front-end

--- router.js
+++ router.js
@@ -1,3 +1,7 @@
 Bustle.Router.map(function(){
   this.resource('news', {path: '/'});
+  this.resource('story1');
+  this.resource('story2');
+  this.resource('story3');
+  this.resource('story4');
 })
